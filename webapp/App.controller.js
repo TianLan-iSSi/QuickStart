d808878140659fbe69290d9dd2f3392dfc17aaad
Tian Lan <tian.lan@iss-i.ca>
2023-08-23 23:12:04 +0000

    step 2

---
+++ webapp/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast"
+], function (Controller, MessageToast) {
+	"use strict";
+
+	return Controller.extend("Quickstart.App", {
+		onPress : function () {
+			MessageToast.show("Hello App!")
+			alert("Hello hello!");
+		}
+	});
+
+});
